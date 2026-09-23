0e8e398aee8c6a6eed7863d53052f6e89f477117
Prashant bhatt <93563996+Prashant2242@users.noreply.github.com>
2026-09-24 00:06:05 +0530

    Update AWS instance configuration in main.tf

--- Project_ec2_instance_creation/main.tf
+++ Project_ec2_instance_creation/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-01a00762f46d584a1"  # Specify an appropriate AMI ID
+    ami = "ami-01a00762f46d58321"  # Specify an appropriate AMI ID
     instance_type = "t3.micro"
-    subnet_id= "subnet-0e5a25695eceaab51"
+    subnet_id= "subnet-0e2a25695eceaab33"
     key_name ="terraform_key"
 } 
